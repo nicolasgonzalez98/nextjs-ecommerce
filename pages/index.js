dce055a4d460cc7a420bed5e3b0c1acb13af1517
Nicolas Nahuel Gonzalez <nicolasgonzalez470@gmail.com>
2023-12-16 21:39:49 -0300

    cambios

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+
+
+export default function Home() {
+  return (
+    <div className="bg-blue-900 w-screen h-screen flex items-center">
+      <div className="text-center w-full">
+        <button className="bg-white p-2 rounded-md">Login with Google</button>
+      </div>
+    </div>
+  )
+}
