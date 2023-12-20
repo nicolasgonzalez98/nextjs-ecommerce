b28a71f4d2d1ecc17fb45aa1ccf09cae612ffc06
Nicolas Nahuel Gonzalez <nicolasgonzalez470@gmail.com>
2023-12-20 00:58:03 -0300

    auth

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,25 @@
-
+import { useSession, signIn, signOut } from "next-auth/react"
 
 export default function Home() {
-  return (
-    <div className="bg-blue-900 w-screen h-screen flex items-center">
-      <div className="text-center w-full">
-        <button className="bg-white p-2 rounded-md">Login with Google</button>
-      </div>
-    </div>
-  )
+
+  const { data: session } = useSession()
+
+  if(!session){
+    return (
+      <div className="bg-blue-900 w-screen h-screen flex items-center">
+        <div className="text-center w-full">
+          
+          <button className="bg-white p-2 px-4 rounded-md" onClick={() => signIn('google')}>Login with Google</button>
+        </div>
+      </div>  
+    )
+  }
+  else{
+    return (
+      <>
+        Signed in as {session.user.email} <br/>
+        <button onClick={() => signOut()}>Sign out</button>
+      </>
+    )
+  }
 }
