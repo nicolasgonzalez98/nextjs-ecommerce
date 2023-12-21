1a33bada027afc7334f02eefddf21e5d1c1d1411
Nicolas Nahuel Gonzalez <nicolasgonzalez470@gmail.com>
2023-12-20 23:57:49 -0300

    cambios layout

---
+++ components/Layout.js
@@ -0,0 +1,28 @@
+import { useSession, signIn, signOut } from "next-auth/react"
+import Nav from "@/components/Nav"
+
+export default function Layout({children}) {
+
+  const { data: session } = useSession()
+
+  if(!session){
+    return (
+      <div className="bg-blue-900 w-screen h-screen flex items-center">
+        <div className="text-center w-full">
+          
+          <button className="bg-white p-2 px-4 rounded-md" onClick={() => signIn('google')}>Login with Google</button>
+        </div>
+      </div>  
+    )
+  }
+  else{
+    return (
+      <div className="bg-blue-900 min-h-screen flex">
+        <Nav/>
+
+        <div className="bg-white flex-grow mt-2 mr-2 rounded-lg p-4 mb-2">{children}<button onClick={() => signOut()}>Sign out</button></div>
+        
+      </div>
+    )
+  }
+}
--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,8 @@
-import { useSession, signIn, signOut } from "next-auth/react"
+import Layout from "@/components/Layout"
 
 export default function Home() {
 
-  const { data: session } = useSession()
-
-  if(!session){
-    return (
-      <div className="bg-blue-900 w-screen h-screen flex items-center">
-        <div className="text-center w-full">
-          
-          <button className="bg-white p-2 px-4 rounded-md" onClick={() => signIn('google')}>Login with Google</button>
-        </div>
-      </div>  
-    )
-  }
-  else{
-    return (
-      <>
-        Signed in as {session.user.email} <br/>
-        <button onClick={() => signOut()}>Sign out</button>
-      </>
-    )
-  }
+  return <Layout>
+    Test
+  </Layout>
 }
