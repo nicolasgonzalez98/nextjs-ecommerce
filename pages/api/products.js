5b840f412f9a28a5ec9aa7221b8104a52b6abc39
Nicolas Nahuel Gonzalez <nicolasgonzalez470@gmail.com>
2023-12-27 00:18:48 -0300

    cambios

--- pages/api/products.js
+++ pages/api/products.js
@@ -1,5 +1,14 @@
-export default function handler(req, res) {
+import clientPromise from "@/lib/mongodb"
+import { Product } from "@/models/Product";
+import mongoose from "mongoose"
+
+export default async function handler(req, res) {
+    mongoose.Promise = clientPromise;
     if (req.method == "POST"){
-        return 
+        const {title, description, price} = req.body;
+        const productDoc = await Product.create({
+            title, description, price
+        })
+        res.json(productDoc)
     }
   }
